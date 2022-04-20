d0842627a546b9fef1132415da7fb5e777f63c80
k-tatsushi <kakeya@craid-inc.com>
2022-04-21 06:07:50 +0900

    test

---
+++ space.php
@@ -0,0 +1,199 @@
+<?php
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
+function divisibleNumbers($a, $b) {
+    $numbers = [];
+    for ($i = $a; $i <= $b; $i++) {
+        if ($i % 4 == 0) {
+            $numbers[] = $i;
+        }
+    }
+    foreach ($numbers as $number) {
+        echo $number . PHP_EOL;
+    }
+}
---
+++ tab.php
@@ -0,0 +1,221 @@
+<?php
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
+function divisibleNumbers($a, $b) {
+	$numbers = [];
+	for ($i = $a; $i <= $b; $i++) {
+		if ($i % 4 == 0) {
+			$numbers[] = $i;
+		}
+	}
+	foreach ($numbers as $number) {
+		echo $number;
+	}
+}
